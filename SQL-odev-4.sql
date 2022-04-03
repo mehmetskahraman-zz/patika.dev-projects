2e2715ebef81fe35bcc7dbdd972b628004dfe2de
mehmetskahraman <101888046+mehmetskahraman@users.noreply.github.com>
2022-04-03 13:40:21 +0300

    Update SQL-odev-4.sql

--- SQL-odev-4.sql
+++ SQL-odev-4.sql
@@ -1,1 +1,21 @@
+--Merhabalar,
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+--Kolay Gelsin.
 
+--film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+select distinct replacement_cost from film;
+
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+select count(distinct replacement_cost) from film;
+
+--film tablosunda bulunan film isimlerinde (title) kaç tanesi T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+select count(title) from film
+where title like 'T%' and rating = 'G';
+
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+select count(country) from country
+where country like '_____';
+
+--city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+select count(city) from city
+where city ilike '%R';
