20d4784459db1825d0e9a644a51adb8c3ad18943
mehmetskahraman <101888046+mehmetskahraman@users.noreply.github.com>
2022-04-03 11:18:38 +0300

    Update SQL-odev-2.sql

--- SQL-odev-2.sql
+++ SQL-odev-2.sql
@@ -1,7 +1,9 @@
 # SQL
   -- 1. film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız 
   -- ( BETWEEN - AND yapısını kullanınız.)
-select * from film where replacement_cost between 12.99 and 16.99;
+select * from film 
+where (replacement_cost between 12.99 and 16.99) and replacement_cost != 16.99 
+order by replacement_cost desc;
 
   -- 2. actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız.
   -- ( IN operatörünü kullanınız.)
