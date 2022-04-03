89adf83f793c356fa55707af8214af67f09a706d
mehmetskahraman <101888046+mehmetskahraman@users.noreply.github.com>
2022-04-03 10:44:39 +0300

    Update SQL-odev-2.sql

--- SQL-odev-2.sql
+++ SQL-odev-2.sql
@@ -1,5 +1,4 @@
-#SQL
---film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız 
---( BETWEEN - AND yapısını kullanınız.)
+# SQL
+  -- film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız 
+  -- ( BETWEEN - AND yapısını kullanınız.)
 ´select * from film where replacement_cost between 12.99 and 16.99;´
-
