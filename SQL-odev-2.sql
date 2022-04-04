c34edd996c65b183c3ad467e6f70dc40793294c8
mehmetskahraman <101888046+mehmetskahraman@users.noreply.github.com>
2022-04-04 22:42:52 +0300

    Update SQL-odev-2.sql

--- SQL-odev-2.sql
+++ SQL-odev-2.sql
@@ -1,3 +1,4 @@
+
 -- 1. film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız
 -- ( BETWEEN - AND yapısını kullanınız.)
 select * from film 
@@ -11,7 +12,6 @@ where first_name in ('Penelope', 'Nick', 'Ed');
 
 -- 3. film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız.
 -- (IN operatörünü kullanınız.)
- 
 select * from film
 where rental_rate in (0.99, 2.99, 4.99) 
 and replacement_cost in (12.99, 15.99, 28.99);
