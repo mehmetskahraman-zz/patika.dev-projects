9c054ef8b588d17dabb9fbd02ec27ce4a5bc3147
mehmetskahraman <101888046+mehmetskahraman@users.noreply.github.com>
2022-04-07 00:20:23 +0300

    Create flatten.py

---
+++ flatten.py
@@ -0,0 +1,14 @@
+list_of_lists = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+flat_list = list()
+def flatten_list(list_of_lists):
+    for item in list_of_lists:
+        if type(item) == list:
+            flatten_list(item)
+        else:
+            flat_list.append(item)
+    return flat_list
+flatten_list(list_of_lists)
+print(flat_list)
+
+for i in reversed(list_of_lists):
+    print(i)
