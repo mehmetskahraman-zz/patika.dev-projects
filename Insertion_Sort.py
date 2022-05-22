1eba672fd2b38acf2b1d3bc847f9afc6c7300c03
mehmetskahraman <101888046+mehmetskahraman@users.noreply.github.com>
2022-05-22 21:35:31 +0300

    Update Insertion_Sort.py

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,12 +1,13 @@
-array = [3, 5, 9, 2, 6, 19]
+new_Array = [3, 5, 9, 2, 6, 19]
 
-def insertion_sort(array):
-    for i in range(1, len(array)):
-        key_item = array[i]
+def insertion_sort(new_Array):
+    '''Applies insertion sort algorithm on a given unsorted array.'''
+    for i in range(1, len(new_Array)):
+        key_item = new_Array[i]
         j = i - 1
-        while j >= 0 and array[j] > key_item:
-            array[j + 1] = array[j]
+        while j >= 0 and new_Array[j] > key_item:
+            new_Array[j + 1] = new_Array[j]
             j -= 1
-            array[j + 1] = key_item
-        return array
-insertion_sort(array)
+            new_Array[j + 1] = key_item
+        return new_Array
+insertion_sort(new_Array)
